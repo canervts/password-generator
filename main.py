1722e2affa6010a95ace40fbeed7ac50aa743deb
canervts <caner.vatansever@gmail.com>
2020-12-18 09:25:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,37 +5,43 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
-nr_letters = int(input("How many letters would you like in your password?\n")) 
+nr_letters= int(input("How many letters would you like in your password?\n")) 
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-#Eazy Level
-# password = ""
-
-# for char in range(1, nr_letters + 1):
-#   password += random.choice(letters)
-
-# for char in range(1, nr_symbols + 1):
-#   password += random.choice(symbols)
-
-# for char in range(1, nr_numbers + 1):
-#   password += random.choice(numbers)
-
-# print(password)
-
-#Hard Level
+#Eazy Level - Order not randomised:
+#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+password = ""
 password_list = []
 
 for char in range(1, nr_letters + 1):
-  password_list.append(random.choice(letters))
+  random_letter = random.choice(letters)
+  #Eazy Level - Order not randomised:
+  password += random_letter
+  #Hard Level - Order of characters randomised:
+  password_list += random_letter
 
 for char in range(1, nr_symbols + 1):
-  password_list += random.choice(symbols)
+  random_symbol = random.choice(symbols)
+  #Eazy Level - Order not randomised:
+  password += random_symbol
+  #Hard Level - Order of characters randomised:
+  password_list += random_symbol
 
 for char in range(1, nr_numbers + 1):
-  password_list += random.choice(numbers)
+  random_number = random.choice(numbers)
+  #Eazy Level - Order not randomised:
+  password += random_number
+  #Hard Level - Order of characters randomised:
+  password_list += random_number
+
+print(f"Your password : {password}")
+
+#Hard Level - Order of characters randomised:
+#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
 
 print(password_list)
+
 random.shuffle(password_list)
 print(password_list)
 
@@ -43,4 +49,4 @@ password = ""
 for char in password_list:
   password += char
 
-print(f"Your password is: {password}")
+print(f"Your shuffle password : {password}")
